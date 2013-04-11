b175529b2638fca606cb088f095c0f772df23ded
Lewis Flude <lewis.flude@gmail.com>
2013-04-12 00:44:57 +0100

    Add initialiser for s3

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -3,7 +3,7 @@ class Vinyl < ActiveRecord::Base
   attr_accessible :title, :artist, :genre, :format, :rating, :album_art, :user_id
   has_attached_file :album_art, 
     :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-  :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :path => "/:style/:id/:filename"
+  :storage => :s3, :s3_credentials => S3_CREDENTIALS, :path => "/:style/:id/:filename"
 
 
   validates_presence_of :title
