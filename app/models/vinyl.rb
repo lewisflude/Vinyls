ecaa16b38540077265961e915b04fa21b94097fa
Lewis Flude <lewis.flude@gmail.com>
2013-04-12 00:37:29 +0100

    Make S3 image upload work locally

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -1,7 +1,10 @@
 class Vinyl < ActiveRecord::Base
 
   attr_accessible :title, :artist, :genre, :format, :rating, :album_art, :user_id
-  has_attached_file :album_art, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :album_art, 
+    :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+  :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :path => "/:style/:id/:filename"
+
 
   validates_presence_of :title
 
