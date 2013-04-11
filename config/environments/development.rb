b57b50e8be790a0bedcb22fd1e269a3b7103d7b4
Lewis Flude <lewis.flude@gmail.com>
2013-04-12 00:21:38 +0100

    Use s3 in development too

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,13 @@ VinylRatings::Application.configure do
   # Config for Paperclip
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
