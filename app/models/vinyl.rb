33c7d7978442d6727222c2b222d7af8165015ad8
Lewis Flude <lewis.flude@gmail.com>
2013-04-12 01:54:53 +0100

    Better styles

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -3,7 +3,8 @@ class Vinyl < ActiveRecord::Base
   attr_accessible :title, :artist, :genre, :format, :rating, :album_art, :user_id
   has_attached_file :album_art, :styles => { 
   :medium => "300x300>", 
-  :thumb => "100x100>" }, 
+  :small => "150x150>",
+  :thumb => "100x100#" }, 
   :storage => :s3, 
   :s3_credentials => S3_CREDENTIALS,
   :s3_host_name  => "s3-eu-west-1.amazonaws.com",
