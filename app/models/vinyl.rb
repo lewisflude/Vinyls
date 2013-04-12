cb227ffcbdfee63cca1024e303bfa7b730413f12
Lewis Flude <lewis.flude@gmail.com>
2013-04-12 02:10:47 +0100

    Temp fix until Paperclip gem gets updated

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -2,9 +2,9 @@ class Vinyl < ActiveRecord::Base
 
   attr_accessible :title, :artist, :genre, :format, :rating, :album_art, :user_id
   has_attached_file :album_art, :styles => { 
-  :medium => "300x300>", 
+  :medium => "300x300!", 
   :small => "150x150>",
-  :thumb => "100x100#" }, 
+  :thumb => "229x229#" }, 
   :storage => :s3, 
   :s3_credentials => S3_CREDENTIALS,
   :s3_host_name  => "s3-eu-west-1.amazonaws.com",
