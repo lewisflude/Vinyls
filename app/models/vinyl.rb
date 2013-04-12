c2da408d0b11614fb6515ab156f5ce5da5aa0228
Lewis Flude <lewis.flude@gmail.com>
2013-04-12 01:39:00 +0100

    FUCK BEING EUROPEAN

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -1,9 +1,13 @@
 class Vinyl < ActiveRecord::Base
 
   attr_accessible :title, :artist, :genre, :format, :rating, :album_art, :user_id
-  has_attached_file :album_art, 
-    :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-  :storage => :s3, :s3_credentials => S3_CREDENTIALS, :path => "/:style/:id/:filename"
+  has_attached_file :album_art, :styles => { 
+  :medium => "300x300>", 
+  :thumb => "100x100>" }, 
+  :storage => :s3, 
+  :s3_credentials => S3_CREDENTIALS,
+  :s3_host_name  => "s3-eu-west-1.amazonaws.com",
+  :path => ":filename"
 
 
   validates_presence_of :title
