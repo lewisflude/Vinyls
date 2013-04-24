60d80b507e2719baaade71d82397f20352c5e31f
Lewis Flude <lewis.flude@gmail.com>
2013-04-24 16:34:48 +0100

    Refactor lastfm fetching

--- lib/lastfm.rb
+++ lib/lastfm.rb
@@ -6,9 +6,9 @@ class Lastfm
   def fetch_album_art(title)
     escaped_title = URI::escape(title)
     lastfm_key = ENV["LASTFM_KEY"]
-    album_search = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=album.search&album=#{escaped_title}&api_key=#{lastfm_key}&format=json")
-    unless results.key?('errors')
-      results = JSON.parse(album_search).fetch('results').fetch('albummatches').fetch('album')[0].fetch('image').last.fetch('#text')
+    query = JSON.parse(RestClient.get("http://ws.audioscrobbler.com/2.0/?method=album.search&album=#{escaped_title}&api_key=#{lastfm_key}&format=json"))
+    unless query.key?('errors')
+      results = query.fetch('results').fetch('albummatches').fetch('album')[0].fetch('image').last.fetch('#text')
     end
   end
 
@@ -16,10 +16,9 @@ class Lastfm
     escaped_artist = URI::escape(artist)
     escaped_title = URI::escape(title)
     lastfm_key = ENV["LASTFM_KEY"]
-    album_info = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist=#{escaped_artist}&album=#{escaped_title}&api_key=#{lastfm_key}&format=json")
-    results = JSON.parse(album_info)
-    unless results.key?('errors')
-       return results.fetch('album').fetch('toptags').fetch('tag')[0].fetch('name')
+    query = JSON.parse(RestClient.get("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist=#{escaped_artist}&album=#{escaped_title}&api_key=#{lastfm_key}&format=json"))
+    unless query.key?('errors')
+       return query.fetch('album').fetch('toptags').fetch('tag')[0].fetch('name')
     end
   end
 end
