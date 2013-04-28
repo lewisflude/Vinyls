1e531705d9fa6aa4c777cfbbbef7a8f3f7d161c4
Lewis Flude <lewis.flude@gmail.com>
2013-04-28 23:03:58 +0100

    Put selections in the right order on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < Clearance::UsersController
   def show
     @user = User.find_by_username(params[:username])
+    @selections = @user.selections.all(order: "created_at DESC", limit: 50)
   end
 end
