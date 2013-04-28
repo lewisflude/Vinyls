2d19e3d3103a266f9230af85d523e821c8566c04
Lewis Flude <lewis.flude@gmail.com>
2013-04-29 00:36:32 +0100

    Use Europa font

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Vinyls::Application.routes.draw do
   root :to => 'selections#index'
 
   get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
-  get '/sign_up' => 'clearance/users#new', as: 'sign_up'
+  get '/join' => 'clearance/users#new', as: 'join'
   delete '/sign_out' => 'clearance/sessions#destroy', as:'sign_out'
   get "/:username", to: "users#show", as: "user"
   # See how all your routes lay out with "rake routes"
