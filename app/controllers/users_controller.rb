986724a3015b06311887f45b2d5c28b03ea0bb1f
Lewis Flude <lewis.flude@gmail.com>
2013-05-03 08:25:28 +0100

    Fix views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < Clearance::UsersController
   def show
     @user = User.find_by_username(params[:username])
-    @selections = @user.selections.all(order: "created_at DESC", limit: 50)
+    @selections = @user.selections.all
   end
 end
